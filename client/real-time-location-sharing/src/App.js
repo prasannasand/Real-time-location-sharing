3bf24c8038faf0ced090eb78066cc630ab32e72c
aprajita279 <156883296+aprajita279@users.noreply.github.com>
2024-05-29 03:18:43 -0700

    Profile removed

--- client/real-time-location-sharing/src/App.js
+++ client/real-time-location-sharing/src/App.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Login from "./components/Login";
 import Register from "./components/Register";
-import Profile from "./components/Profile";
 import Geofencing from "./components/Geofencing";
 import ManageMembers from "./components/ManageMembers";
 
@@ -15,7 +14,6 @@ function App() {
 				<Route path="/" element={<Home />} exact />
 				<Route path="/login" element={<Login />} />
 				<Route path="/register" element={<Register />} />
-				<Route path="/Profile" element={<Profile />} />
 				<Route path="/Geofencing" element={<Geofencing />} />
 				<Route path="/ManageMembers" element={<ManageMembers />} />
 			</Routes>
