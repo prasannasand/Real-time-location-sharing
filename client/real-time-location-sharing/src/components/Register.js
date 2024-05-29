cb7f7f635fc37b857e398c4d80e1808075e30bf9
aprajita279 <156883296+aprajita279@users.noreply.github.com>
2024-05-28 22:13:36 -0700

    Login and register enhancements completed

--- client/real-time-location-sharing/src/components/Register.js
+++ client/real-time-location-sharing/src/components/Register.js
@@ -1,57 +1,88 @@
-// components/Register.js
-import React, { useState } from "react";
+import { useState } from "react";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faUser, faEnvelope, faEye, faEyeSlash, faKey } from '@fortawesome/free-solid-svg-icons';
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import "./Register.css";
 
 function Register() {
-	const [username, setUsername] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+  const [error, setError] = useState("");
+  const navigate = useNavigate();
 
-	const handleSubmit = async (event) => {
-		event.preventDefault();
-		try {
-			const response = await axios.post(
-				"http://localhost:8080/register",
-				{ username, email, password }
-			);
-			console.log(response.data); // Handle registration response
-		} catch (error) {
-			console.error("Registration error", error.response);
-		}
-	};
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/register",
+        { username, email, password }
+      );
+      console.log(response.data);
+      if (response.data === "User registered successfully") {
+        navigate("/login");
+      } else {
+        setError(response.data);
+      }
+    } catch (error) {
+      console.error("Registration error", error.response);
+      setError("An error occurred during registration. Please try again.");
+    }
+  };
 
-	return (
-		<div>
-			<h1>Register</h1>
-			<form onSubmit={handleSubmit}>
-				<label>
-					Username:
-					<input
-						type="text"
-						value={username}
-						onChange={(e) => setUsername(e.target.value)}
-					/>
-				</label>
-				<label>
-					Email:
-					<input
-						type="email"
-						value={email}
-						onChange={(e) => setEmail(e.target.value)}
-					/>
-				</label>
-				<label>
-					Password:
-					<input
-						type="password"
-						value={password}
-						onChange={(e) => setPassword(e.target.value)}
-					/>
-				</label>
-				<button type="submit">Register</button>
-			</form>
-		</div>
-	);
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+  };
+
+  return (
+    <div className="register-container">
+      <h1>Register</h1>
+      {error && <p className="error-message">{error}</p>}
+      <form className="register-form" onSubmit={handleSubmit}>
+        <label>
+            <FontAwesomeIcon icon={faUser} className="icon" />
+            Username
+          </label>
+        <div className="form-group">
+          <input
+            type="text"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+        </div>
+        <label>
+            <FontAwesomeIcon icon={faEnvelope} className="icon" />
+            Email
+          </label>
+        <div className="form-group">
+          <input
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <label>
+            <FontAwesomeIcon icon={faKey} className="icon" />
+            Password
+          </label>
+        <div className="form-group password-input">
+          <input
+            type={showPassword ? "text" : "password"}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <FontAwesomeIcon
+            icon={showPassword ? faEyeSlash : faEye}
+            className="icon"
+            onClick={togglePasswordVisibility}
+          />
+        </div>
+        <button type="submit">Register</button>
+      </form>
+    </div>
+  );
 }
 
 export default Register;
